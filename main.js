92bce38734f47810172db3498f422812e68d331d
Tomas Nazar <tommi.nazar@gmail.com>
2022-07-11 00:22:39 -0300

    algo de cartas

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
+alert("cuantas cartas quieres que haya en la mesa")
+let cartas = prompt("repartire una a una cuantas quieras, dime un numero")
 
-alert("llegaremos a cero")
-alert("desde que numero quieres volver");
-let numero = prompt ("ingrese un numero entre 1 y 9");
-
-if (numero > 9){
-    alert("ingresaste mal el numero sonso, es del 1 al 9")
+if(cartas>52){
+    alert("hay solo 52 cartas en la baraja de poker")
 }
-
-if (numero <= 9){
-    alert("cuenta regresiva")
-    for(i = numero ; i > 0 ; i--){
+if (cartas <=52){
+    alert("ok entonces repartire " + cartas + " cartas en la mesa");
+    for( i=0 ; i != cartas ; i++){
+        alert(i+1)
         
-        alert (i);   
-    } 
-    alert("bien echo!!!")
+    }
+    alert("de todas esas cartas elige una")
 }
 
 
 
+// alert("llegaremos a cero")
+// alert("desde que numero quieres volver");
+// let numero = prompt ("ingrese un numero entre 1 y 9");
 
-// if (texto == "tomas"){
-//     alert ( "tu nombre es tomas");
-// }else{
-//     alert ("lindo nombre")
+// if (numero > 9){
+//     alert("ingresaste mal el numero sonso, es del 1 al 9")
+// }
+
+// if (numero <= 9){
+//     alert("cuenta regresiva")
+//     for(i = numero ; i > 0 ; i--){
+        
+//         alert (i);   
+//     } 
+//     alert("bien echo!!!")
 // }
